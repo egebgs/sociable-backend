479c80413bce7dd9e52a4f682373d13ad806482e
Ege Bağırsakçı <egebagirsakci@sabanciuniv.edu>
2023-06-17 20:53:01 +0300

    Implemented Login and Tokenization

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,8 +1,9 @@
 const asyncHandler = require('express-async-handler');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const User = require("../models/userModel");
 //@desc Register a user
-//@route POST /api/auth/register
+//@route POST /api/user/register
 //@access Public
 
 const registerUser = asyncHandler(async (req, res) => {
@@ -46,15 +47,35 @@ const registerUser = asyncHandler(async (req, res) => {
 });
 
 //@desc Login a user
-//@route POST /api/auth/login
+//@route POST /api/user/login
 //@access Public
 const loginUser = asyncHandler(async (req, res) => {
-    res.json({message: "Login route"});
+    const {email, password} = req.body;
+    if(!email || !password){
+        res.status(400);
+        throw new Error("Please enter all fields");
+    }
+    const user = await User.findOne({email});
+    if(user && (await bcrypt.compare(password, user.password))){
+        const accessToken = jwt.sign({
+            user: {
+                username: user.username,
+                email: user.email,
+                id: user.id
+            }
+        }, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1h'});
+        res.status(200).json({accessToken});
+
+    }
+    else{
+        res.status(401);
+        throw new Error("Invalid email or password");
+    }
 });
 
 
 //@desc Retrieve current user info
-//@route POST /api/auth/current
+//@route POST /api/user/current
 //@access Public
 
 const currentUser = asyncHandler(async (req, res) => {
